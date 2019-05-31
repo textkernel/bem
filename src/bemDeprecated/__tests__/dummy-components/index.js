47f1a935db09f8e166e71e1eabce9e1b2edefb84
Alexander Antipin <alex@antipin.com>
2019-06-01 01:36:07 +0200

    feat: removed bemDeprecated

--- src/bemDeprecated/__tests__/dummy-components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './Button';
-export { default as ButtonStateless } from './ButtonStateless';
-export { default as Avatar } from './Avatar';
-export { default as AvatarStateless } from './AvatarStateless';
