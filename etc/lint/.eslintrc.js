5729f1dac5081e2ab9dfa830ee539476c985cc5b
Alexander Antipin <alex@antipin.com>
2019-06-05 00:37:16 +0200

    style: set up eslint configuration

---
+++ etc/lint/.eslintrc.js
@@ -0,0 +1,57 @@
+module.exports =  {
+    parser: '@typescript-eslint/parser',
+    parserOptions:  {
+        warnOnUnsupportedTypeScriptVersion: false,
+        project: 'tsconfig.json',
+        ecmaVersion:  2018,
+        sourceType:  'module',
+        ecmaFeatures:  {
+            jsx: true,
+        },
+    },
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends:  [
+        'airbnb',
+        'plugin:jest/recommended',
+        // 'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    settings:  {
+        // react:  {
+        //     version:  'detect',
+        // },
+        'import/resolver': {
+            'node': {
+            'paths': ['src'],
+            'extensions': ['.ts', '.tsx']
+            }
+        }
+    },
+    rules:  {
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/interface-name-prefix': 'always',
+    },
+    overrides: [
+        {
+            files: ['*.spec.ts'],
+            rules: {
+               '@typescript-eslint/explicit-function-return-type': 'off',
+            },
+        },
+        {
+            files: ['**/etc/**/*.ts', '**/etc/**/*.js'],
+            rules: {
+               'import/no-extraneous-dependencies': 'off',
+            },
+        },
+        {
+            files: ['index.ts'],
+            rules: {
+               'import/prefer-default-export': 'off',
+            },
+        }
+    ],
+};
